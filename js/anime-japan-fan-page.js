3552dc0ee3532b756829ebca14e73514b84cc2b7
Walter Smith <walter.c.smith01@gmail.com>
2020-12-22 18:16:50 -0500

    Anime-Japan-Fan-page:Created an welcome prompt followed by a confirm, an console log if conditionals and alerts

--- js/anime-japan-fan-page.js
+++ js/anime-japan-fan-page.js
@@ -6,26 +6,47 @@
 
 // alert("Welcome to the page!");
 
-
-var firstPart = "Hey visitor(s)! How are you?";
-console.log(firstPart);
-//
-// Realized why my alerts, prompts, or console logs would repeat at least twice because
-// I had two script tags with src to anime-japan-fan-page.js 😝
-// face with stuck out tongue and tightly-closed eyes
-// Unicode: U+1F61D, UTF-8: F0 9F 98 9D
-
 var userInput = prompt("Welcome visitor! What is your name: ");
 alert("Nice to meet you, " + userInput);
 
-console.log("Nice to meet you, " + userInput);
+var likePage = confirm("Do you like the page?");
+console.log(likePage);
 
-var confirmed = confirm(userInput +", Do you want to view the page?");
-console.log(confirmed);
-
-function confirm() {
-    
+if(likePage === true){
+    console.log("Thanks for liking the page!");
+}else {
+    console.log("It's a shame you don't like the page...");
 }
+
+alert("I'm going to ask you two more questions, " + userInput);
+
+
+var areYouSure = confirm("Are you SURE you like the page? Be honest!");
+
+if(areYouSure === true){
+    var answer = prompt("Are you interested in coding, (yes or no)?")
+    if (answer === 'yes' || answer === 'Yes' || answer === 'YES'){
+        alert("That's fantastic!");
+    }else {
+        alert("Well, not everyone likes coding! Thanks for the answer tho!");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 
