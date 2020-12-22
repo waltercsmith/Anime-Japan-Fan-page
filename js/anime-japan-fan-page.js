2ed080b5193870c44c40ac0338c4090f22c21944
Walter Smith <walter.c.smith01@gmail.com>
2020-12-21 19:48:30 -0500

    Anime-Japan-Fan-page:Added a prompt and alert and console welcoming the visitor, asking for their name and if they want to view the page

--- js/anime-japan-fan-page.js
+++ js/anime-japan-fan-page.js
@@ -1,5 +1,29 @@
 "use strict"
 
-var userInput = prompt('What is your name:');
-alert("Nice to me you, " + userInput)
+// var sayHey = "Hey, visitor!";
+// console.log(sayHey);
+// alert(sayHey);
+
+// alert("Welcome to the page!");
+
+
+var firstPart = "Hey visitor(s)! How are you?";
+console.log(firstPart);
+//
+// Realized why my alerts, prompts, or console logs would repeat at least twice because
+// I had two script tags with src to anime-japan-fan-page.js 😝
+// face with stuck out tongue and tightly-closed eyes
+// Unicode: U+1F61D, UTF-8: F0 9F 98 9D
+
+var userInput = prompt("Welcome visitor! What is your name: ");
+alert("Nice to meet you, " + userInput);
+
+console.log("Nice to meet you, " + userInput);
+
+var confirmed = confirm(userInput +", Do you want to view the page?");
+console.log(confirmed);
+
+
+
+
 
